cf795adf3ae03d63cfa053bd88e10963d50e94eb
NickMurphy324 <97997827+NickMurphy324@users.noreply.github.com>
2022-01-18 19:13:16 -0600

    Add files via upload

---
+++ wdv341/helloWorld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="wdith=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>WDV341 Intro PHP</h1>
+    <h2>Hello World</h2>
+    <?php
+        echo "<h3>HELLO WORLD! from PHP</h3>";
+    ?>
+</body>
+</html>
