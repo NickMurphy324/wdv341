4f96cd64688bbd5d7e4569212d8561cd1fb73afe
NickMurphy324 <97997827+NickMurphy324@users.noreply.github.com>
2022-02-01 18:14:55 -0600

    Add files via upload

---
+++ wdv341/unit3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+$yourName = "Nick Murphy";
+echo "<h1>WDV 341 - Intro to PHP</h1>";
+?>
+</head>
+<body>
+<h2>
+    <?php
+    echo $yourName;
+    ?>
+</h2>
+</body>
+</html>
